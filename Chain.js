7b0db5d8fe9e80ebafc09a186fa1d41ae103b6a0
Sweetu23 <73287289+Sweetu23@users.noreply.github.com>
2020-12-23 16:47:14 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,13 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var options ={
+            bodyA:bodyA,bodyB:bodyB,
+            stiffness: 0.04,length:10
+        }
+        this.rope = Constraint.create(options)
+World.add(world,this.rope)
+    }
+    display(){
+    line(this.rope.bodyA.position.x,this.rope.bodyA.position.y,this.rope.bodyB.position.x,this.rope.bodyB.position.y)
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AngryBird-Stage3
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
